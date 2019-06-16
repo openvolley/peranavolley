1787c64fab4d0e1b363e9968b36ba88f0953206d
Ben Raymond <ben@theraymonds.org>
2019-06-16 11:58:55 +1000

    really fix reexports

--- R/peranavolley.R
+++ R/peranavolley.R
@@ -6,6 +6,7 @@
 #' @author Ben Raymond \email{ben@@untan.gl}
 #' @docType package
 #' @importFrom assertthat assert_that is.flag is.string
+#' @importFrom datavolley home_team inspect teams visiting_team
 #' @importFrom dplyr as_tibble bind_rows case_when distinct everything filter lag lead left_join mutate rename select tibble tribble %>%
 #' @importFrom lubridate as.duration hms ymd_hms
 #' @importFrom rlang .data
