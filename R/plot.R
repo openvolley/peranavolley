3d3fa893944807dc965cc7b807d40cd49e3d864e
Ben Raymond <ben@theraymonds.org>
2020-01-26 18:27:39 +1100

    don't redefine funcs from datavolley, just import them

--- R/plot.R
+++ R/plot.R
@@ -1,28 +1,3 @@
-## funcs from datavolley
-dv_xy_xbins <- function() 0.5+(seq_len(100) - 11) / 80 * 3.0
-dv_xy_ybins <- function() 0.5+(seq_len(101) - 11) / 81 * 6.0
-
-dv_xy2index <- function(x, y) {
-    if (missing(y) && ncol(x) > 1) {
-        y <- x[, 2]
-        x <- x[, 1]
-    }
-    assert_that(is.numeric(x))
-    assert_that(is.numeric(y))
-    ## define cells (these are LEFT edges) in plot x, y space
-    ##binx <- seq(from=3*(1-10.5)/79+0.5, to=3*(100-10.5)/79+0.5, length.out=100)
-    binx <- dv_xy_xbins()
-    binx[1] <- -Inf ## so that anything to the left of the first cell is put in the first cell
-    binx <- c(binx, Inf) ## and anything beyond the last cell is put into the last cell
-    ##biny <- seq(from=3*(1-10.5)/40.5+0.5, to=3*(101-10.5)/40.5+0.5, length.out=101)
-    biny <- dv_xy_ybins()
-    biny[1] <- -Inf
-    biny <- c(biny, Inf)
-    xi <- .bincode(x, binx, right = FALSE)
-    yi <- .bincode(y, biny, right = FALSE)
-    as.integer(xi + (yi - 1) * (length(binx) - 1))
-}
-
 xy2zone <- function(x, y, as_for_serve = FALSE) {
     assert_that(is.flag(as_for_serve), !is.na(as_for_serve))
     if (!as_for_serve) {
